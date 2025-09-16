5d9316050cb6fdd18cb6f8b51befdd50121dd3ca
karolrdg <karolinerodrigues12@outlook.com>
2025-09-16 20:21:53 -0300

    Adicionar arquivos de projeto.

---
+++ SegundoProjeto/Instituicao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoProjeto
+{
+    internal class Instituicao
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+        public Departamento[] Departamentos { get; } = new Departamento[5];
+
+        private int quantidadeDepartamentos = 0;
+
+        public void RegistrarDepartamento(Departamento d)
+        {
+            if (quantidadeDepartamentos < 10)
+                Departamentos[quantidadeDepartamentos++] = d;
+        }
+
+        public int ObterQuantidadeDepartamentos()
+        {
+            return quantidadeDepartamentos;
+        }
+    }
+
+}
---
+++ SegundoProjeto/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace SegundoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iesUTFPR = new Instituicao();
+            iesUTFPR.Nome = "UTFPR";
+            iesUTFPR.Endereco = "Medianeira";
+
+            var iesCC = new Instituicao();
+            iesCC.Nome = "Casa do Código";
+            iesCC.Endereco = "São Paulo";
+
+            var dptoEnsino = new Departamento();
+            dptoEnsino.Nome = "Computação";
+
+            var dptoAlimentos = new Departamento();
+            dptoAlimentos.Nome = "Alimentos";
+
+            var dptoRevisao = new Departamento();
+            dptoRevisao.Nome = "Revisão Escrita";
+
+            iesUTFPR.RegistrarDepartamento(dptoEnsino);
+            iesUTFPR.RegistrarDepartamento(dptoAlimentos);
+
+            iesCC.RegistrarDepartamento(dptoRevisao);
+
+            Console.WriteLine("UTFPR");
+            for (int i = 0; i < iesUTFPR.ObterQuantidadeDepartamentos(); i++)
+            {
+                Console.WriteLine($"==> {iesUTFPR.Departamentos[i].Nome}");
+            }
+
+            Console.WriteLine("Casa do Código");
+            for (int i = 0; i < iesCC.ObterQuantidadeDepartamentos(); i++)
+            {
+                Console.WriteLine($"==> {iesCC.Departamentos[i].Nome}");
+            }
+            Console.Write("Pressione qualquer tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
